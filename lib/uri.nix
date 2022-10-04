0792775843d89e45647ea4205987021367a06084
aameen-tulip <98349328+aameen-tulip@users.noreply.github.com>
2022-10-03 22:04:34 -0500

    Update uri.nix

--- lib/uri.nix
+++ lib/uri.nix
@@ -47,7 +47,6 @@
         t = lib.libtag.verifyTag x;
         s = if builtins.isString x then x else
             if t.isTag && ( t.name == "scheme" ) then t.val else ( x.val or x );
-
       in if builtins.isString s then UriScheme.fromString s else
          UriScheme.fromAttrs s;
     in defun [( either string ( with libyants; attrs any ) ) ut.Structs.scheme]
@@ -69,45 +68,36 @@
     name = "Url";
     isType = with libyants; defun [any bool] ( Url.ytype.check );
     ytype = libyants.either ut.Strings.uri ut.Structs.url;
-    X = ( lib.libtypes.defXTypes {
-      inherit (libyants) any;
-      string = ut.Strings.uri;
-      attrs  = ut.Structs.url;
-      this   = Url.ytype;
-    } {
-
-      # Writer
-      this.string = x: let
+    # Writer
+    toString = let
+      inner = x: let
         auth = if ( x.authority or null ) == null then "" else
                "/${x.authority}";
+        # FIXME: query lacks toString
         q = if ( x.query or null ) != null then "?${x.query}" else "";
         frag = if ( x.fragment or null ) == null then "" else
                "#${x.fragment}";
         mp = if ( x.path or null ) == null then "" else x.path;
-      in "${x.scheme}:${auth}${mp}${q}${frag}";
-
-      # Deserializer
-      #attrs.this = a: null;
-
-      # Serializer
-      #this.attrs = x: null;
-
-      # Coercer
-      #any.this = x: null;
-    } ) // {
-      # Parser ( already wrapped with type checking )
-      string.this = lib.parser.parseFullUrl;
-    };
+        fa = "${x.scheme}:${auth}${mp}${q}${frag}";
+      in if builtins.isString then x else fa;
+    in defun [Url.ytype ut.Strings.uri] inner;
+    # Deserializer
+    #fromAttrs = a: null;
+    # Serializer
+    #toAttrs = x: null;
+    # Coercer
+    #coerceUrl = x: null;
+    # Parser ( already wrapped with type checking )
+    fromString = lib.parser.parseFullUrl;
     # Object Constructor
     __functor = self: x: {
       _type      = "Url";
-      val        = self.X.any.this x;
-      __toString = child: self.X.this.string child.val;
-      #__serial   = child: self.X.this.attrs  child.val;
+      val        = self.coerceUrl x;
+      __toString = child: self.toString child.val;
+      #__serial   = child: self.toAttrs child.val;
       __vtype    = self.ytype;
     };
   };
-
 
 
 # ---------------------------------------------------------------------------- #
