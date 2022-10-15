44c277c5986e88afee449012990548d3f7275a1a
Alex Ameen <alex.ameen.tx@gmail.com>
2022-10-15 16:15:41 -0500

    use character classes in RE

--- re/uri.nix
+++ re/uri.nix
@@ -10,16 +10,10 @@ let
 
   # Character Classes Enumerated
   character_classes = rec {
-    digit_c      = "0123456789";
-    upper_c      = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    lower_c      = "abcdefghijklmnopqrstuvwxyz";
-    alpha_c      = lower_c + upper_c;
-    alnum_c      = alpha_c + digit_c;
-    word_c       = alnum_c + "-";
-    hex_c        = digit_c + "abcdefABCDEF";
-    escaped_c    = "%" + hex_c;
+    aldash_c     = "[:alnum:]-";
+    escaped_c    = "%[:xdigit:]";
     mark_c       = "_.!~*'()-";       # XXX: be careful with "-" in any `[...]'
-    unreserved_c = alnum_c + mark_c;  # XXX: mark_c
+    unreserved_c = "[:alnum:]${mark_c}";  # XXX: mark_c
     res_ns_c     = ";?:@&=+$,";
     reserved_c   = res_ns_c + "/";
     # These use `escaped_c' and require "pseudo character classes"
@@ -47,10 +41,10 @@ let
       seg = "([1-9][[:digit:]]?[[:digit:]]?|0)";
     in builtins.concatStringsSep "\\." [seg seg seg seg];
     ipv6_addr_p = let
-      seg = "[0-9a-fA-F:]+(%[${word_c}]+)?";
+      seg = "[0-9a-fA-F:]+(%[${aldash_c}]+)?";
     in "([[]${seg}[]]|${seg})";
-    top_label_p    = "[[:alpha:]]([${alnum_c}-]*[[:alnum:]])?";
-    domain_label_p = "[[:alnum:]]([${alnum_c}-]*[[:alnum:]])?";
+    top_label_p    = "[[:alpha:]]([${aldash_c}]*[[:alnum:]])?";
+    domain_label_p = "[[:alnum:]]([${aldash_c}]*[[:alnum:]])?";
     hostname_p     = "(${domain_label_p}\\.)*${top_label_p}\\.?";
     host_p         = "(${hostname_p}|${ipv4_addr_p}|${ipv6_addr_p})";
     hostport_p     = "${host_p}(:${port_p})?";
@@ -58,8 +52,8 @@ let
     server_p       = "((${userinfo_p}@)?${hostport_p})?";
     reg_name_p     = "1(${escaped_p1}|[$,;:@&=+${unreserved_c}])*";
     authority_p    = "(${server_p}|${reg_name_p})";
-    scheme_p       = "[[:alpha:]][${alnum_c}+.-]*";
-    layer_p        = "[[:alpha:]][${alnum_c}.-]*";  # scheme sublayer "()+()"
+    scheme_p       = "[[:alpha:]][[:alnum:]+.-]*";
+    layer_p        = "[[:alpha:]][[:alnum:].-]*";  # scheme sublayer "()+()"
     rel_segment_p  = "1(${escaped_p1}|[;@&=+$,${unreserved_c}])*";
     rel_path_p     = "${rel_segment_p}(${abs_path_p})?";
     abs_path_p     = "/${path_segments_p}";
@@ -70,7 +64,7 @@ let
     abs_uri_p  = "${scheme_p}:(${hier_part_p}|${opaque_part_p})";
     uri_ref_p  = "(${abs_uri_p}|${rel_uri_p})?(#${fragment_p})?";
 
-    flake_id_p = "[[:alpha:]][${word_c}]*";
+    flake_id_p = "[[:alpha:]][${aldash_c}]*";
   };
 
 
