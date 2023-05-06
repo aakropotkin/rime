ad295ef8b72b3a489712e07c2f8eddf47d9ee932
Alex Ameen <alex.ameen.tx@gmail.com>
2023-05-06 18:12:38 -0500

    add nix-parse-uri

---
+++ src/parse-uri/default.nix
@@ -0,0 +1,8 @@
+{ nixpkgs       ? builtins.getFlake "nixpkgs"
+, system        ? builtins.currentSystem
+, pkgsFor       ? nixpkgs.legacyPackages.${system}
+, stdenv        ? pkgsFor.stdenv
+, nix           ? pkgsFor.nix
+, boost         ? pkgsFor.boost
+, nlohmann_json ? pkgsFor.nlohmann_json
+}: import ./pkg-fun.nix { inherit stdenv nix boost nlohmann_json; }
---
+++ src/parse-uri/main.cc
@@ -0,0 +1,32 @@
+
+#include <cstddef>
+#include <iostream>
+#include <string>
+#include <nix/url.hh>
+#include <nlohmann/json.hpp>
+
+  int
+main( int argc, char * argv[], char ** envp )
+{
+  for ( int i = 1; i < argc; i++ )
+    {
+      nix::ParsedURL       url    = nix::parseURL( argv[i] );
+      nix::ParsedUrlScheme scheme = nix::parseUrlScheme( url.scheme );
+
+      auto      res    = nlohmann::json::object();
+      res["base"]      = url.base;
+      res["scheme"]    = url.scheme;
+      res["authority"] = url.authority.value_or( "" );
+      res["path"]      = url.path;
+      res["fragment"]  = url.fragment;
+      res["query"]     = nlohmann::json::object();
+      for ( const auto & e : url.query )
+        {
+          res["query"][e.first] = e.second;
+        }
+      res["application-layer"] = scheme.application.value_or( "" );
+      res["translport-layer"]  = scheme.transport;
+      std::cout << res.dump() << std::endl;
+    }
+  return 0;
+}
