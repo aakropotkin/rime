d6fac1c1186cdc563565213b196996d96e1912d4
Alex Ameen <alex.ameen.tx@gmail.com>
2023-05-14 09:43:35 -0500

    Update main.cc

--- src/parse-uri/main.cc
+++ src/parse-uri/main.cc
@@ -12,18 +12,20 @@ main( int argc, char * argv[], char ** envp )
     {
       nix::ParsedURL       url    = nix::parseURL( argv[i] );
       nix::ParsedUrlScheme scheme = nix::parseUrlScheme( url.scheme );
-
-      auto      res    = nlohmann::json::object();
+      auto                 res    = nlohmann::json::object();
+    
       res["base"]      = url.base;
       res["scheme"]    = url.scheme;
       res["authority"] = url.authority.value_or( "" );
       res["path"]      = url.path;
       res["fragment"]  = url.fragment;
       res["query"]     = nlohmann::json::object();
+    
       for ( const auto & e : url.query )
         {
           res["query"][e.first] = e.second;
         }
+    
       res["application-layer"] = scheme.application.value_or( "" );
       res["translport-layer"]  = scheme.transport;
       std::cout << res.dump() << std::endl;
