f177fb17ab4fb3c4a9a8389de829f5ae42db00c8
Alex Ameen <alex.ameen.tx@gmail.com>
2023-05-16 07:43:30 -0500

    update README

--- src/resolve/main.cc
+++ src/resolve/main.cc
@@ -9,20 +9,20 @@
 
 #include <nlohmann/json.hpp>
 
-using namespace nix;
-
   int
 main( int argc, char * argv[], char ** envp )
 {
-  initNix();
-  initGC();
+  nix::initNix();
+  nix::initGC();
 
-  evalSettings.pureEval = false;
+  nix::evalSettings.pureEval = false;
 
-  EvalState state( {}, openStore() );
+  nix::EvalState state( {}, nix::openStore() );
 
-  auto originalRef = parseFlakeRef( argv[1], absPath( "." ) );
-  auto resolvedRef = originalRef.resolve( state.store );
+  nix::FlakeRef originalRef =
+    nix::parseFlakeRef( argv[1], nix::absPath( "." ) );
+
+  nix::FlakeRef resolvedRef = originalRef.resolve( state.store );
   
   std::cout << resolvedRef.to_string() << std::endl;
 
